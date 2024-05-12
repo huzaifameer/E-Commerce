e2638458fb574274ecc7421ed8eb9865a1fae206
Huzaifa Ameer <113491165+huzaifameer@users.noreply.github.com>
2024-05-12 23:18:10 +0530

    part-day-1-done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import {Product} from "../src/Pages/Product.jsx"
 import {Cart} from "../src/Pages/Cart.jsx"
 import {LoginSignUp} from "../src/Pages/LoginSignUp.jsx"
 import { Footer } from './components/Footer/Footer.jsx';
+import men_banner from "./components/Assets/banner_mens.png"
+import women_banner from "./components/Assets/banner_women.png"
+import kids_banner from "./components/Assets/banner_kids.png"
+
 
 function App() {
   return (
@@ -15,9 +19,9 @@ function App() {
         <NavBar/>
         <Routes>
           <Route path='/' element={<Shop/>}/>
-          <Route path='/men' element={<ShopCategory category = "men" />}/>
-          <Route path='/women' element={<ShopCategory category = "women" />}/>
-          <Route path='/kids' element={<ShopCategory category = "kids" />}/>
+          <Route path='/men' element={<ShopCategory banner={men_banner} category = "men" />}/>
+          <Route path='/women' element={<ShopCategory banner={women_banner} category = "women" />}/>
+          <Route path='/kids' element={<ShopCategory banner={kids_banner} category = "kids" />}/>
           <Route path='/product' element={<Product/>}>
             <Route path='productId' element={<Product/>} />
           </Route>
