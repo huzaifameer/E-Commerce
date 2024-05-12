2201ea35847f5f1170e455e699af66603e82b5fa
Huzaifa Ameer <113491165+huzaifameer@users.noreply.github.com>
2024-05-12 12:49:27 +0530

    commit-hero

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Hero } from '../components/Hero/Hero'
 
 export const Shop = () => {
   return (
     <div>
-        Shop
+        <Hero/>
     </div>
   )
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Hero.css";
+import hand_icon from "../Assets/hand_icon.png";
+import arrowIcon from "../Assets/arrow.png";
+import heroImage from "../Assets/hero_image.png";
+
+export const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-left">
+        <h2>New Arrivals Only</h2>
+        <div>
+          <div className="hero-hand-icon">
+            <p>new</p>
+            <img src={hand_icon} alt="" />
+          </div>
+          <p>Collections</p>
+          <p>For Everyone</p>
+        </div>
+        <div className="hero-latest-btn">
+          <div>Latest Collection</div>
+          <img src={arrowIcon} alt="hero" />
+        </div>
+      </div>
+      <div className="hero-right">
+        <img src={heroImage} alt="" />
+      </div>
+    </div>
+  );
+};
