dbabfe2c59673ea798cc8b08eb3123053b4d054a
Huzaifa Ameer <113491165+huzaifameer@users.noreply.github.com>
2024-05-12 18:32:07 +0530

    commit-br-2

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Hero } from '../components/Hero/Hero'
 import Popular from '../components/Popular/Popular'
+import { Offers } from '../components/Offers/Offers'
+import { NewCollections } from '../components/NewCollections/NewCollections'
 
 export const Shop = () => {
   return (
     <div>
         <Hero/>
         <Popular/>
+        <Offers/>
+        <NewCollections/>
     </div>
   )
 }
